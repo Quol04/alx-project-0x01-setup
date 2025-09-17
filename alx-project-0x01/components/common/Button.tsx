609dd6c9d6ac74ad5e3aeb0196fbf6179ad17985
quol <kuolamol77@gmail.com>
2025-09-17 13:26:56 +0300

    completed task2

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,10 @@
+
+const Button : React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Button
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,11 @@
 import Header from "@/components/layout/Header";
+
+const User : React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default User ;
